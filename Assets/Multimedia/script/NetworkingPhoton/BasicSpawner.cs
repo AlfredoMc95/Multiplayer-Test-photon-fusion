0b27141cc3ab8c1cb7187cd550f0609b4139cdc9
AlfredoMc95 <alfredomartcam@gmail.com>
2024-02-06 15:09:37 -0500

    bug
    "solución" bug cama  cambia de personaje tratar de instaciar la camara via codigo

--- Assets/Multimedia/script/NetworkingPhoton/BasicSpawner.cs
+++ Assets/Multimedia/script/NetworkingPhoton/BasicSpawner.cs
@@ -17,11 +17,14 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {
 
-        // Create a unique position for the player
-        Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 1, 0);
-        NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, UtilstSpawn.GetRancomSpawnPoint(), Quaternion.identity, player);
-        // Keep track of the player avatars for easy access
-        _spawnedCharacters.Add(player, networkPlayerObject);
+        if (runner.IsServer)
+        {
+            // Create a unique position for the player
+            Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 1, 0);
+            NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+            // Keep track of the player avatars for easy access
+            _spawnedCharacters.Add(player, networkPlayerObject);
+        }
     }
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
