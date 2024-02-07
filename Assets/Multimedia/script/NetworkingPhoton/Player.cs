0f862e45c245acb0d24479def821aad1d0e60a0a
AlfredoMc95 <alfredomartcam@gmail.com>
2024-02-06 20:07:34 -0500

    conflicts

--- Assets/Multimedia/script/NetworkingPhoton/Player.cs
+++ Assets/Multimedia/script/NetworkingPhoton/Player.cs
@@ -32,8 +32,12 @@ public class Player : NetworkBehaviour
             {
                 if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON0))
                 {
+<<<<<<< HEAD
                     //cadencia de un segundo
                     delay = TickTimer.CreateFromSeconds(Runner, 1f);
+=======
+                    delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+>>>>>>> parent of 8e1223b (3person camera, bug)
                     Runner.Spawn(_prefabBall,
                     transform.position + _forward, Quaternion.LookRotation(_forward),
                     Object.InputAuthority, (runner, o) =>
